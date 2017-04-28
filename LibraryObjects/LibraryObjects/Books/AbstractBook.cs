8e51589f0bba990d1d4677531e13358cdbe98c6b
KobernykYura <kobernykbeljr@gmail.com>
2017-04-28 23:16:43 +0300

    Start
    Project about library service. Using EF, ADO.net, Services.

---
+++ LibraryObjects/LibraryObjects/Books/AbstractBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Library
+{
+    /// <summary>
+    /// Абстрактный класс книг.
+    /// </summary>
+    public abstract class AbstractBook
+    {
+        enum Rating
+        {
+            terrible,
+            onestar,
+            twostars,
+            threestars,
+            fourstars,
+            fivestars,
+            extrachoice
+        }
+        enum CoverQuality
+        {
+            Leather,
+            Soft,
+            Hard
+        }
+
+        public int pages { get; }
+        public string BookName { get; }
+        public string BookAuthor { get; }
+        public string PubLHouse { get; }
+        public string UrlAddress { get; }
+        public decimal Cost { get; }
+
+        public abstract void Add();
+        public abstract void Delete();
+        public abstract void Change();
+
+    }
+}
---
+++ LibraryObjects/WebService/Data/DataBase.cs
@@ -0,0 +1,31 @@
+﻿namespace WebService.Data
+{ 
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DataBase : DbContext
+    {
+        // Контекст настроен для использования строки подключения "DataBase" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "WebЫукмшсу.Data.DataBase" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "DataBase" 
+        // в файле конфигурации приложения.
+        public DataBase()
+            : base("name=DataBase")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
