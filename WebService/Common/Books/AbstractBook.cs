57f31f7973cac20206fbf1e3d0e1a14db33688cb
KobernykYura <kobernykbeljr@gmail.com>
2017-05-01 10:52:29 +0300

    Rebuilding the project
    New version of project.

---
+++ WebService/Common/Books/AbstractBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Books;
+
+namespace Common
+{
+    abstract public class AbstractBook<T>
+    {
+        List<BookRating> rating;
+
+        enum BookRating
+        {
+            zerostar,
+            twostar,
+            threestar,
+            fourstar,
+            fivestar,
+            expra
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
+        public BookType TypeB { get; set; }
+    }
+}
---
+++ WebService/Common/Books/BookType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Books
+{
+    public class BookType
+    {
+        public int ID { get; set; }
+        public string BTypeName { get; set; }
+    }
+}
